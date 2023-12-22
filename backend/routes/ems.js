abe6f462f81d7e38e810acff602f16dc951a97ac
Archishmaan <archishmaan.upa4@gmail.com>
2023-12-22 12:55:24 +0530

    created display and add enployee APIs

--- backend/routes/ems.js
+++ backend/routes/ems.js
@@ -15,4 +15,27 @@ dbClient.connect(dbUrl,(err,connection)=>{
     }
 })
 
+router.get("/display",(req,res)=>{
+    dbConnection.collection("employee").find({}).toArray((err,data)=>{
+        if(err){
+            console.log("Cannot fetch employee..");
+        }
+        else{
+            res.send(data)
+        }
+    })
+})
+
+router.post("/add",(req,res)=>{
+    dbConnection.collection("employee").find({empid:req.body.empid}).toArray((err,data)=>{
+        if(err){
+            console.log("Cannot add employee");
+        }
+        else{
+            dbConnection.collection("employee").insertOne({...req.body})
+            res.send("Inserted employee...")
+        }
+    })
+})
+
 module.exports = router
